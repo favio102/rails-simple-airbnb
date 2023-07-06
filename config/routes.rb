d55317e021387c3bc1ed13d3458b49a5d4f71e78
Favio Valero <favio102@protonmail.com>
2023-07-05 23:12:57 -0700

    CRUD added

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
   get 'flats', to: 'flats#index'
+  get 'flats/new', to: 'flats#new'
+  post 'flats', to: 'flats#create'
+  get 'flats/:id', to: 'flats#show', as: :flat
+  get 'flats/:id/edit', to: 'flats#edit', as: :flats_edit
+  patch 'flats/:id', to: 'flats#update'
+  delete 'flats/:id', to: 'flats#destroy'
 end
